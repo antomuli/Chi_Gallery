ba1aae2dda4fde3d540579a26847840ea3f780d4
Anthony Muli <mulianthony561@gmail.com>
2020-02-29 10:04:49 +0300

    Connected the view function to the specific URLs for instance URL for search,category and location

--- photos/urls.py
+++ photos/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.my_photos, name='myPhotos'),
-    url(r'^search/', views.search_results, name='search_results'),
+    # url(r'^search/', views.search_results, name='search_results'),
     url(r'^category/(\d+)', views.get_category, name = 'get_category'),
     url(r'^location/(\d+)', views.get_location, name = 'get_location')
 ]
